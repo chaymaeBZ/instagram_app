f88e5d7daea44a5122a88b7b1c9cb016fc5fec9f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-26 10:56:32 +0000

    bug fixes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     #flash[:green] = "Authenticated to Facebook !"
-    redirect_to create_url
+    render text: request.env['omniauth.auth'].to_yaml
   end
   def twitter
     render text: request.env['omniauth.auth'].to_yaml
