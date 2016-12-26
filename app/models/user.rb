ddf059a861426a7324c20ddddccb1ec8287260fd
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-26 20:35:10 +0000

    added indexes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable,
          :omniauthable, :omniauth_providers => [:facebook, :twitter]
+
+
+
 end
