c6993785111aba749eefcade8d5de5f7e3f7bb64
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-26 10:37:21 +0000

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook, :twitter]
 end
