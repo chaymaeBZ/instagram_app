13621b41218d12a6f2ed99c985f77fd43a10ba06
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 11:09:22 +0000

    bug fixes

--- db/migrate/20161225162256_devise_create_users.rb
+++ db/migrate/20161225162256_devise_create_users.rb
@@ -41,5 +41,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
+    add_index :users, :uid
+    add_index :users, :provider
+    add_index :users, [:uid, :provider], unique: true
+  end
   end
 end
--- db/migrate/20161225194147_add_omniauth_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddOmniauthToUsers < ActiveRecord::Migration[5.0]
-  def change
-  end
-  add_index :users, :uid
-  add_index :users, :provider
-  add_index :users, [:uid, :provider], unique: true
-end
