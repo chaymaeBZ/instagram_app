18ef7c3088e49085bbe5a2eca6878c483670b9bb
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 10:38:07 +0000

    twitter registration

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :fetch_user
   def facebook
     #flash[:green] = "Authenticated to Facebook !"
     render text: request.env['omniauth.auth'].to_yaml
   end
   def twitter
-    render text: request.env['omniauth.auth'].to_yaml
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      flash[:green] = "Authenticated to Twitter !"
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
+  def failure
+    redirect_to root_path
+  end
+
+  private
+    def fetch_user
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+    end
 end
