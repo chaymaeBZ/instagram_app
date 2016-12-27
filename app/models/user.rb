90cee0193e5dcf347a5e1f15285b8f8d83d35a7f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 12:27:43 +0000

    skiping confirmations when authenticating via twitter or facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable,
           :omniauthable, :omniauth_providers => [:facebook, :twitter]
   class << self
+
     def from_omniauth(auth_hash)
       user = find_or_create_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
       user.name = auth_hash[:info][:name]
       user.email = auth_hash[:info][:email]
       user.skip_confirmation!
+      user.save!
       user
     end
   end
