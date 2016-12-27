3ad91f7562848b98f09eac856e54cb48e24627ef
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 11:03:33 +0000

    bug fixes

--- db/migrate/20161225194147_add_omniauth_to_users.rb
+++ db/migrate/20161225194147_add_omniauth_to_users.rb
@@ -3,7 +3,5 @@ class AddOmniauthToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :provider, :string
     add_column :users, :uid, :string
   end
-  add_index :users, :uid
-  add_index :users, :provider
-  add_index :users, [:uid, :provider], unique: true
+  
 end
