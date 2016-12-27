1d1a3423d0425026f94f2c6ece80580fa8e61f28
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 11:05:21 +0000

    bug fixes

--- db/migrate/20161225162256_devise_create_users.rb
+++ db/migrate/20161225162256_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :name,               null: false, default: ""
+      t.string :uid
+      t.string :provider
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/migrate/20161225194147_add_omniauth_to_users.rb
+++ db/migrate/20161225194147_add_omniauth_to_users.rb
@@ -1,7 +1,7 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
   end
-  
+  add_index :users, :uid
+  add_index :users, :provider
+  add_index :users, [:uid, :provider], unique: true
 end
