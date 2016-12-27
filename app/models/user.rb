e97b4e67e85e4dfb69d063994ef3c67510185f2f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-12-27 11:19:54 +0000

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable,
-         :omniauthable, :omniauth_providers => [:facebook, :twitter]
-
-  class << self
-    def from_omniauth(auth_hash)
-      user = find_or_create_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
-      user.name = auth_hash[:info][:name]
-      user.email = auth_hash[:info][:email]
-      user.save!
-      user
-    end
-  end
-
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable, :omniauthable,
+         :omniauth_providers => [:facebook, :twitter]
 end
